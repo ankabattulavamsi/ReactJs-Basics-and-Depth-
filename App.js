6396b6730293a8beff6648a16cdc58c20c050a84
Vamsi Ankabattula <vamsi@Vamsis-MacBook-Air.local>
2024-11-11 00:55:51 +0530

    exprimental with JSX and functional Componenets

--- App.js
+++ App.js
@@ -3,10 +3,31 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", { id: "heading" }, "Hi Reactjs");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
 const headingjsx = <h1 id="heading">Hi this jsx element</h1>;
 
-console.log(headingjsx);
+const elem = <h3>Hi this an element</h3>;
 
-root.render(headingjsx);
+const TitleElem = () => <h4>i'm from title TitleElem</h4>;
+
+const TitleComponent = () => <h2>i'm from title TitleComponent</h2>;
+
+const TitleComponent2 = () => (
+  <h2>i'm from title TitleComponent2 to access fun expression</h2>
+);
+
+//Component Composition
+const HeadingComponent = () => {
+  return (
+    <div>
+      {elem}
+      <TitleComponent />
+      <TitleElem></TitleElem>
+      {TitleComponent2()}
+      <h1 className="heading">Hello from JSX </h1>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
