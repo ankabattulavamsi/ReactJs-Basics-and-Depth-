6160169b63f9bb5137c042adafcd9acd6fab427a
Vamsi Ankabattula <vamsi@Vamsis-MacBook-Air.local>
2024-11-10 21:10:53 +0530

    Created elaments using React create element and render using React dom

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-// <div id='parent'>
-// <div id='child'>
-//     <h1>im h1 tag</h1>
-//     <h2>im h2 tag</h2>
-// </div>
-
-// <div id='child'>
-//     <h1>im h1 tag</h1>
-//     <h2>im h2 tag</h2>
-// </div>
-// </div>
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "im h1 tag"),
-    React.createElement("h2", {}, "im h2 tag"),
+    React.createElement("h1", {}, "Hi i'm Vamsi"),
+    React.createElement("h2", {}, "im Ankabattula"),
   ]),
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "im h1 tag"),
